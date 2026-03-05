ae2fe3064b56c2ab2146810537bb0b81a5ca1628
Leandro Júnior <leandrojr28102006@gmail.com>
2026-03-05 14:34:28 -0400

    Assistindo a segunda aula de AED (continuação)

--- Algoritmo e Estruturas de Dados 1/aula2aed.cpp
+++ Algoritmo e Estruturas de Dados 1/aula2aed.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 
+// int main(){
+// 	std::string nome=" ";
+// 	int idade=0;
+// 	std::cout<<"Digite seu nome e sua idade: "<<std::endl;
+// 	std::cin>>nome>>idade;
+
+// 	std::cout<<"Seu nome: "<<nome<<std::endl;
+// 	std::cout<<"Sua idade: "<<idade<<std::endl;
+// }
+
